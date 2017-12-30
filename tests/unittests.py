c6f7a476c76e117126165c4375c0a3c73174ee47
Joe Moore <jpvlsmv@gmail.com>
2017-12-30 10:25:38 -0500

    Testing

--- tests/unittests.py
+++ tests/unittests.py
@@ -3,6 +3,25 @@ from .context import archstore
 
 import unittest
 
+class WalkerTest(unittest.TestCase):
+  def test1(self):
+    from .context.archstore import walkers
+    w = walkers.Walker()
+    self.assert(isinstance(w,walkers.Walker))
+
+class localfsWalkerTest(unittest.TestCase):
+  def test1(self):
+    from .context.archstore import walkers
+    w = walkers.localfsWalker()
+    self.assert(isinstance(w,walkers.localfsWalker))
+    self.assert(isinstance(w,walkers.Walker))
+  def test2(self):
+    import tempfile
+    from .context.archstore import walkers
+    w = walkers.localfsWalker()
+    with tempfile.TemporaryDir() as T:
+      self.assert(w.walk(Path(T)) == [ ] )
+
 class ArchstoreTest(unittest.TestCase):
   """Basic test cases."""
   def test1(self):
